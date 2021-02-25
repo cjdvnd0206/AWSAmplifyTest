d004aea2f1b426c124014bcad43938156e8a48c0
cjdvnd0206 <cjdvndaudnjf@naver.com>
2021-02-25 09:47:15 +0900

    Initial Commit

---
+++ AWSAmplifyTest/AWSAmplifyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSAmplifyTestApp.swift
+//  AWSAmplifyTest
+//
+//  Created by user on 2021/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AWSAmplifyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
